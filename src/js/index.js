742eb76b6b905c8c5a51c703b78aa7e9b281f9fb
aecordova <ae_cordova@outlook.com>
2020-06-19 13:08:41 -0500

    Add loaders to webpack config and file structure for styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,13 @@
+import '../style/main.scss';
 import getW from './getWeather';
 
-const cityName = 'london';
+
+const wBox = document.querySelector('.weather-container'); 
+
+const cityName = 'guadalupe';
 
 const displayWeather = async () => {
+  const data = await getW(cityName);
+  wBox.textContent = JSON.stringify(data, null, 2);
 };
-
 displayWeather();
--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,47 @@ module.exports = {
   plugins: [
     // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'WeatherApp',
+      template: './src/index.html',
     }),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ttf|woff|woff2|eot|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run postcss actions
+          options: {
+            plugins() { // postcss plugins, can be exported to postcss.config.js
+              return [
+                // eslint-disable-next-line global-require
+                require('autoprefixer'),
+              ];
+            },
+          },
+        }, {
+          loader: 'sass-loader', // compiles Sass to CSS
+        }],
+      },
+    ],
   },
 };
