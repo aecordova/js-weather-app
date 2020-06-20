a7898a2e4228b301b23e6dd6e10a24af55a411ad
aecordova <ae_cordova@outlook.com>
2020-06-20 14:32:45 -0500

    Add error validation and response format to fetching data

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,22 @@
 import '../style/main.scss';
 // import make from './make';
 
-// import getW from './getWeather';
-// const wBox = document.querySelector('.weather-container'); 
-// const cityName = 'guadalupe';
-// const displayWeather = async () => {
-//   const data = await getW(cityName);
-//   wBox.textContent = JSON.stringify(data, null, 2);
-// };
+import getW from './getWeather';
+const searchForm = document.querySelector('.search-frm');
+const searchInput = document.querySelector('.search-inp');
+const wBox = document.querySelector('.weather-container');
+
+const displayWeather = async (city) => {
+  const data = await getW(city);
+  wBox.textContent = JSON.stringify(data, null, 2);
+  console.log(data);
+};
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const cityName = searchInput.value.toLowerCase();
+  displayWeather(cityName);
+});
+
+
 // displayWeather();
