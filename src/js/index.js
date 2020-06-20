48ca501d86990a41a56a0804b99eae1d81797a45
aecordova <ae_cordova@outlook.com>
2020-06-19 19:10:27 -0500

    Add header with  search form, all styled up

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,11 @@
 import '../style/main.scss';
-import getW from './getWeather';
+// import make from './make';
 
-
-const wBox = document.querySelector('.weather-container'); 
-
-const cityName = 'guadalupe';
-
-const displayWeather = async () => {
-  const data = await getW(cityName);
-  wBox.textContent = JSON.stringify(data, null, 2);
-};
-displayWeather();
+// import getW from './getWeather';
+// const wBox = document.querySelector('.weather-container'); 
+// const cityName = 'guadalupe';
+// const displayWeather = async () => {
+//   const data = await getW(cityName);
+//   wBox.textContent = JSON.stringify(data, null, 2);
+// };
+// displayWeather();
