0d102922aae612e227bfb155a3e5d5bf51c9150b
aecordova <ae_cordova@outlook.com>
2020-06-22 18:24:01 -0500

    Add function to update icon and connected UI to API

--- src/js/getWeather.js
+++ src/js/getWeather.js
@@ -1,33 +1,49 @@
 const APIKEY = '1c46b4274489cf56fe2ee767e9586191';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+const ICON_URL = 'https://openweathermap.org/img/wn/02d@4x.png'
 
-const buildRequest = (city) => [BASE_URL, `?q=${city}`, `&appid=${APIKEY}`].join('');
+const buildRequest = (city, units = 'metric') => [BASE_URL, `?q=${city}`, `&units=${units}`, `&appid=${APIKEY}`].join('');
+const handleError = (response) => {
+  let msg;
+  if (response.status === 404) {
+    msg = 'Could not find a city with that name...';
+  } else {
+    msg = response.statusText;
+  }
+  throw new Error(msg);
+};
 
-const getWeatherData = async (city) => {
-  const data = await fetch(buildRequest(city))
-    .then((r) => {
-      let msg;
+const getIcon = async (iconName) => {
+  const icon = await fetch(`https://openweathermap.org/img/wn/${iconName}@4x.png`)
+    .then(r => {
       if (!r.ok) {
-        if (r.status === 404) {
-          msg = 'Could not find a city with that name...';
-        } else {
-          msg = r.statusText;
-        }
-        throw new Error(msg);
+        handleError(r);
+      }
+      return r.blob();
+    })
+    .then(URL.createObjectURL);
+  return icon;
+};
+
+const getWeatherData = async (city, units = 'metric') => {
+  const data = await fetch(buildRequest(city, units))
+    .then((r) => {
+      if (!r.ok) {
+        handleError(r);
       }
       return r.json();
     })
     .then((d) => ({
-      weather: {
-        type: d.weather[0].main,
-        desc: d.weather[0].description,
-      },
+      city: d.name,
+      country: d.sys.country,
+      weather: d.weather,
       temp: d.main,
-      sunset: d.sys.sunset,
-      sunrise: d.sys.sunrise,
     }))
     .catch((e) => e.message);
-  return data;
+
+    const icon = await getIcon(data.weather[0].icon);
+
+  return { data, icon };
 };
 
 export default getWeatherData;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,27 @@
 import '../style/main.scss';
 import getW from './getWeather';
+
 const searchForm = document.querySelector('.search-frm');
 const searchInput = document.querySelector('.search-inp');
-const wBox = document.querySelector('.weather-container');
+
+const cityDisplay = document.querySelector('.disp-city');
+const tempDisplay = document.querySelector('.disp-temp');
+const descDisplay = document.querySelector('.disp-desc');
+const feelsDisplay = document.querySelector('.disp-fls');
+const minDisplay = document.querySelector('.disp-min');
+const maxDisplay = document.querySelector('.disp-max');
+const wIcon = document.querySelector('.disp-icon');
 
 const displayWeather = async (city) => {
-  const data = await getW(city);
-  wBox.textContent = JSON.stringify(data, null, 2);
+  const w = await getW(city);
+  cityDisplay.textContent = [w.data.city, w.data.country].join(', ');
+  tempDisplay.textContent = Math.trunc(w.data.temp.temp);
+  descDisplay.textContent = w.data.weather[0].description;
+  feelsDisplay.textContent = Math.trunc(w.data.temp.feels_like);
+  minDisplay.textContent = Math.trunc(w.data.temp.temp_min);
+  maxDisplay.textContent = Math.trunc(w.data.temp.temp_max);
+  wIcon.style.backgroundImage = `url(${w.icon})`;
+  console.log(w.icon);
 };
 
 searchForm.addEventListener('submit', (e) => {
