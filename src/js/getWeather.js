0190e064c4b394f05b5b692a31e058a36032643c
aecordova <ae_cordova@outlook.com>
2020-06-23 19:25:16 -0500

    remove unnecessary files

--- src/js/getWeather.js
+++ src/js/getWeather.js
@@ -1,13 +1,11 @@
-const APIKEY = "1c46b4274489cf56fe2ee767e9586191";
-const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-const ICON_URL = "https://openweathermap.org/img/wn/02d@4x.png";
+const APIKEY = '1c46b4274489cf56fe2ee767e9586191';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-const buildRequest = (city, units = "metric") =>
-  [BASE_URL, `?q=${city}`, `&units=${units}`, `&appid=${APIKEY}`].join("");
+const buildRequest = (city, units = 'metric') => [BASE_URL, `?q=${city}`, `&units=${units}`, `&appid=${APIKEY}`].join('');
 const handleError = (response) => {
   let msg;
   if (response.status === 404) {
-    msg = "Could not find a city with that name...";
+    msg = 'Could not find a city with that name...';
   } else {
     msg = response.statusText;
   }
@@ -16,7 +14,7 @@ const handleError = (response) => {
 
 const getIcon = async (iconName) => {
   const icon = await fetch(
-    `https://openweathermap.org/img/wn/${iconName}@4x.png`
+    `https://openweathermap.org/img/wn/${iconName}@4x.png`,
   )
     .then((r) => {
       if (!r.ok) {
@@ -28,7 +26,7 @@ const getIcon = async (iconName) => {
   return icon;
 };
 
-const getWeatherData = async (city, units = "metric") => {
+const getWeatherData = async (city, units = 'metric') => {
   const data = await fetch(buildRequest(city, units))
     .then((r) => {
       if (!r.ok) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,60 @@
 // eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/js/index.js",
+  entry: './src/js/index.js',
   plugins: [
     // new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.(ttf|woff|woff2|eot|otf)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.svg$/,
-        loader: "svg-inline-loader",
+        loader: 'svg-inline-loader',
       },
       {
         test: /\.s[ac]ss$/,
         use: [
           {
-            loader: "style-loader", // inject CSS to page
+            loader: 'style-loader', // inject CSS to page
           },
           {
-            loader: "css-loader", // translates CSS into CommonJS modules
+            loader: 'css-loader', // translates CSS into CommonJS modules
           },
           {
-            loader: "postcss-loader", // Run postcss actions
+            loader: 'postcss-loader', // Run postcss actions
             options: {
               plugins() {
                 // postcss plugins, can be exported to postcss.config.js
                 return [
                   // eslint-disable-next-line global-require
-                  require("autoprefixer"),
+                  require('autoprefixer'),
                 ];
               },
             },
           },
           {
-            loader: "sass-loader", // compiles Sass to CSS
+            loader: 'sass-loader', // compiles Sass to CSS
           },
         ],
       },
